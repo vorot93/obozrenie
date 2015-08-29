c2f340dd4e72be570ee1e8c399e5ce0657cae7cc
Artem Vorotnikov <artem@vorotnikov.me>
2015-08-29 05:44:14 +0300

    [Urwid frontend] Stub

---
+++ obozrenie/tui.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# This source file is part of Obozrenie
+# Copyright 2015 Artem Vorotnikov
+
+# For more information, see https://github.com/skybon/obozrenie
+
+# Obozrenie is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as
+# published by the Free Software Foundation.
+
+# Obozrenie is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Obozrenie.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Simple and easy to use game server browser."""
+
+import argparse
+import ast
+import os
+import shutil
+import threading
+
+import urwid
+from gi.repository import GLib
+
+from obozrenie import helpers
+from obozrenie.core import Core, Settings
+import obozrenie.gtk_templates as templates
+from obozrenie.global_settings import *
