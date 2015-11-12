24b84b7ab35f53559d4402ae038a58b09b32d74d
Artem Vorotnikov <artem@vorotnikov.me>
2015-11-12 22:25:32 +0300

    TDD: Merge units into test class

--- tests/tests.py
+++ tests/tests.py
@@ -23,53 +23,53 @@ import unittest
 from obozrenie import helpers, backends
 
 
-def unit_dict_to_list():
-    """Does helper convert dict table into a list table based on specified format correctly?"""
-    spec_table = [{'a': 'A1', 'b': 'B1', 'c': 'C1', 'd': 'D1', 'e': 'E1'}, {'d': 'D2', 'e': 'E2', 'a': 'A2'}]
-    spec_format = ('a',
-                   'e',
-                   'd')
-    spec_result = [['A1', 'E1', 'D1'], ['A2', 'E2', 'D2']]
-
-    result = helpers.dict_to_list(spec_table, spec_format)
-
-    return {'expectation': spec_result, 'result': result}
-
-
-def unit_flatten_dict_table():
-    """Does helper flatten dict based on specified format correctly?"""
-    spec_table = {'lead1': {'a': 'A1', 'b': 'B1', 'c': 'C1', 'd': 'D1', 'e': 'E1'}, 'lead2': {'a': 'A2', 'b': 'B2'}}
-    leading_key_spec = 'leading key'
-    spec_result = [{'leading key': 'lead1', 'a': 'A1', 'b': 'B1', 'c': 'C1', 'd': 'D1', 'e': 'E1'}, {'leading key': 'lead2', 'a': 'A2', 'b': 'B2'}]
-
-    result = helpers.flatten_dict_table(spec_table, leading_key_spec=leading_key_spec)
-
-    return {'expectation': spec_result, 'result': result}
-
-
-def unit_sort_dict_table():
-    """Checks how helper function sorts dictionary tables"""
-    spec_table = [{'name': 'Olga', 'age': 32, 'gender': 'F'}, {'name': 'Marina', 'age': 25, 'gender': 'F'}, {'name': 'Nikolai', 'age': 40, 'gender': 'M'}]
-    spec_key = 'name'
-    spec_result = [{'name': 'Marina', 'age': 25, 'gender': 'F'}, {'name': 'Nikolai', 'age': 40, 'gender': 'M'}, {'name': 'Olga', 'age': 32, 'gender': 'F'}]
-
-    result = helpers.sort_dict_table(spec_table, spec_key)
-
-    return {'expectation': spec_result, 'result': result}
-
-
 class HelpersTests(unittest.TestCase):
     """Tests for helpers"""
+    @staticmethod
+    def unit_dict_to_list():
+        """Does helper convert dict table into a list table based on specified format correctly?"""
+        spec_table = [{'a': 'A1', 'b': 'B1', 'c': 'C1', 'd': 'D1', 'e': 'E1'}, {'d': 'D2', 'e': 'E2', 'a': 'A2'}]
+        spec_format = ('a',
+                   'e',
+                   'd')
+        spec_result = [['A1', 'E1', 'D1'], ['A2', 'E2', 'D2']]
+
+        result = helpers.dict_to_list(spec_table, spec_format)
+
+        return {'expectation': spec_result, 'result': result}
+
     def test_dict_to_list(self):
-        unit = unit_dict_to_list()
+        unit = self.unit_dict_to_list()
         self.assertTrue(unit['expectation'] == unit['result'])
+
+    @staticmethod
+    def unit_flatten_dict_table():
+        """Does helper flatten dict based on specified format correctly?"""
+        spec_table = {'lead1': {'a': 'A1', 'b': 'B1', 'c': 'C1', 'd': 'D1', 'e': 'E1'}, 'lead2': {'a': 'A2', 'b': 'B2'}}
+        leading_key_spec = 'leading key'
+        spec_result = [{'leading key': 'lead1', 'a': 'A1', 'b': 'B1', 'c': 'C1', 'd': 'D1', 'e': 'E1'}, {'leading key': 'lead2', 'a': 'A2', 'b': 'B2'}]
+
+        result = helpers.flatten_dict_table(spec_table, leading_key_spec=leading_key_spec)
+
+        return {'expectation': spec_result, 'result': result}
 
     def test_flatten_dict_table(self):
-        unit = unit_flatten_dict_table()
+        unit = self.unit_flatten_dict_table()
         self.assertTrue(unit['expectation'] == unit['result'])
 
+    @staticmethod
+    def unit_sort_dict_table():
+        """Checks how helper function sorts dictionary tables"""
+        spec_table = [{'name': 'Olga', 'age': 32, 'gender': 'F'}, {'name': 'Marina', 'age': 25, 'gender': 'F'}, {'name': 'Nikolai', 'age': 40, 'gender': 'M'}]
+        spec_key = 'name'
+        spec_result = [{'name': 'Marina', 'age': 25, 'gender': 'F'}, {'name': 'Nikolai', 'age': 40, 'gender': 'M'}, {'name': 'Olga', 'age': 32, 'gender': 'F'}]
+
+        result = helpers.sort_dict_table(spec_table, spec_key)
+
+        return {'expectation': spec_result, 'result': result}
+
     def test_sort_dict_table(self):
-        unit = unit_sort_dict_table()
+        unit = self.unit_sort_dict_table()
         self.assertTrue(unit['expectation'] == unit['result'])
 
 
