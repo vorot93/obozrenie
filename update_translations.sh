3c8d84cc24825c51a9592c539478697aa9cc263c
Artem Vorotnikov <artem@vorotnikov.me>
2026-06-18 00:59:00 +0300

    Modernize build: replace distutils setup.py with pyproject.toml

--- update_translations.sh
+++ update_translations.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DOMAIN='me.vorotnikov.Obozrenie'
 LOCALEDIR='locale'
-python3 setup.py extract_messages -o $LOCALEDIR/$DOMAIN.pot
-python3 setup.py update_catalog -i $LOCALEDIR/$DOMAIN.pot -D $DOMAIN -d $LOCALEDIR
+pybabel extract -o $LOCALEDIR/$DOMAIN.pot obozrenie
+pybabel update -i $LOCALEDIR/$DOMAIN.pot -D $DOMAIN -d $LOCALEDIR
